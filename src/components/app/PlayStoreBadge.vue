c1c10147b8bf44430d6c7ba97a0993444f0bb817
Ryan Stewart <stewart1233@hotmail.co.uk>
2026-03-28 17:36:42 +0000

    Update DuskScreen screenshots, coming soon badge, remove lock screen feature

--- src/components/app/PlayStoreBadge.vue
+++ src/components/app/PlayStoreBadge.vue
@@ -4,11 +4,16 @@ const badgeSrc = `${import.meta.env.BASE_URL}images/google-play-badge.png`
 </script>
 
 <template>
-  <a :href="url" target="_blank" rel="noopener noreferrer" class="inline-block transition-opacity hover:opacity-80">
-    <img
-      :src="badgeSrc"
-      alt="Get it on Google Play"
-      class="h-14"
-    />
-  </a>
+  <div v-if="url">
+    <a :href="url" target="_blank" rel="noopener noreferrer" class="inline-block transition-opacity hover:opacity-80">
+      <img
+        :src="badgeSrc"
+        alt="Get it on Google Play"
+        class="h-14"
+      />
+    </a>
+  </div>
+  <div v-else class="inline-block rounded-full border border-outline-variant/30 px-6 py-3 font-label text-sm text-on-surface-variant">
+    Coming soon to Google Play
+  </div>
 </template>
